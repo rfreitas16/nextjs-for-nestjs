74ee3d39cad3cd547d75c139d6493d4e2ed780f0
rafael <rfreitas16@hotmail.com>
2025-12-19 14:32:19 -0300

    validando form do user

---
+++ src/actions/user/create-user-action.ts
@@ -0,0 +1,49 @@
+'use server';
+
+import {
+  CreateUserSchema,
+  PublicUserDto,
+  PublicUserSchema,
+} from '@/lib/user/schemas';
+import { asyncDelay } from '@/utils/async-delay';
+import { getZodErrorMessages } from '@/utils/get-zod-error-messages';
+
+type CreateUserActionState = {
+  user: PublicUserDto;
+  errors: string[];
+  success: boolean;
+};
+
+export async function createUserAction(
+  state: CreateUserActionState,
+  formData: FormData,
+): Promise<CreateUserActionState> {
+  await asyncDelay(3000);
+
+  if (!(formData instanceof FormData)) {
+    return {
+      user: state.user,
+      errors: ['Dados inválidos'],
+      success: false,
+    };
+  }
+
+  const formObj = Object.fromEntries(formData.entries());
+  const parsedFormData = CreateUserSchema.safeParse(formObj);
+
+  if (!parsedFormData.success) {
+    return {
+      user: PublicUserSchema.parse(formObj),
+      errors: getZodErrorMessages(parsedFormData.error.format()),
+      success: false,
+    };
+  }
+
+  // FETCH API
+
+  return {
+    user: state.user,
+    errors: [],
+    success: true,
+  };
+}
