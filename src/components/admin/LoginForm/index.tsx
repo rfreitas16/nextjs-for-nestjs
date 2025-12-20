22d3736f38bb865aa246fde4a886fcb31daa8e1c
rafael <rfreitas16@hotmail.com>
2025-12-20 10:39:02 -0300

    migrando o login pra api

--- src/components/admin/LoginForm/index.tsx
+++ src/components/admin/LoginForm/index.tsx
@@ -12,15 +12,15 @@ import { toast } from 'react-toastify';
 
 export function LoginForm() {
   const initialState = {
-    username: '',
-    error: '',
+    email: '',
+    errors: [],
   };
   const [state, action, isPending] = useActionState(loginAction, initialState);
 
   useEffect(() => {
-    if (state.error) {
+    if (state.errors.length > 0) {
       toast.dismiss();
-      toast.error(state.error);
+      state.errors.forEach(e => toast.error(e));
     }
   }, [state]);
 
@@ -34,11 +34,12 @@ export function LoginForm() {
       <form action={action} className='flex-1 flex flex-col gap-6'>
         <InputText
           type='text'
-          name='username'
-          labelText='Usuário'
-          placeholder='Seu usuário'
+          name='email'
+          labelText='Email'
+          placeholder='Seu email'
           disabled={isPending}
-          defaultValue={state.username}
+          defaultValue={state.email}
+          required
         />
 
         <InputText
@@ -47,6 +48,7 @@ export function LoginForm() {
           labelText='Senha'
           placeholder='Sua senha'
           disabled={isPending}
+          required
         />
 
         <Button disabled={isPending} type='submit' className='mt-4'>
@@ -58,7 +60,7 @@ export function LoginForm() {
           <Link href='/user/new'>Criar minha conta</Link>
         </p>
 
-        {!!state.error && <p className='text-red-600'>{state.error}</p>}
+        {/* {!!state.error && <p className='text-red-600'>{state.error}</p>} */}
       </form>
     </div>
   );
