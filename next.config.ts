f208b89d36e8be849f3445d8fd537fc66d7ab276
rafael <rfreitas16@hotmail.com>
2025-12-22 13:54:17 -0300

    criando a rota de upload de imagem

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
       {
         protocol: 'http',
         hostname: 'localhost',
-        port: '3000',
+        port: '3001',
         pathname: '/**',
         search: '',
       },
--- src/actions/upload/upload-image-action.ts
+++ src/actions/upload/upload-image-action.ts
@@ -1,8 +1,7 @@
 'use server';
 
-import { verifyLoginSession } from '@/lib/login/manage-login';
-import { mkdir, writeFile } from 'fs/promises';
-import { extname, resolve } from 'path';
+import { getLoginSessionForApi } from '@/lib/login/manage-login';
+import { authenticatedApiRequest } from '@/utils/authenticated-api-request';
 
 type UploadImageActionResult = {
   url: string;
@@ -14,7 +13,7 @@ export async function uploadImageAction(
 ): Promise<UploadImageActionResult> {
   const makeResult = ({ url = '', error = '' }) => ({ url, error });
 
-  const isAuthenticated = await verifyLoginSession();
+  const isAuthenticated = await getLoginSessionForApi();
   if (!isAuthenticated) {
     return makeResult({ error: 'Faca login novamente' });
   }
@@ -33,20 +32,18 @@ export async function uploadImageAction(
   if (!file.type.startsWith('image/')) {
     return makeResult({ error: 'Imagem invalida' });
   }
-  const ImageExtension = extname(file.name);
-  const uniqueImageName = `${Date.now()}${ImageExtension}`;
-  const uploadDir = process.env.IMAGE_UPLOAD_DIRECTORY || 'uploads';
+  const uploadResponse = await authenticatedApiRequest<{ url: string }>(
+    `/upload`,
+    {
+      method: 'POST',
+      body: formData,
+    },
+  );
+  if (!uploadResponse.success) {
+    return makeResult({ error: uploadResponse.errors[0] });
+  }
 
-  const uploadFullPath = resolve(process.cwd(), 'public', uploadDir);
-  await mkdir(uploadFullPath, { recursive: true });
-
-  const fileArrayBuffer = await file.arrayBuffer();
-  const buffer = Buffer.from(fileArrayBuffer);
-  const fileFullPath = resolve(uploadFullPath, uniqueImageName);
-  await writeFile(fileFullPath, buffer);
-  const imgServerUrl =
-    process.env.IMAGE_SERVER_URL || 'http://localhost:3000/uploads';
-  const url = `${imgServerUrl}/${uniqueImageName}`;
+  const url = `${process.env.IMAGE_SERVER_URL}${uploadResponse.data.url}`;
 
   return makeResult({ url });
 }
